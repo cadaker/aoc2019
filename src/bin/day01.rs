c5f3a90e4e21cd025fffbc5b651005f5a8ec824e
Christian Adåker <cadaker@users.noreply.github.com>
2019-12-01 21:52:24 +0100

    Day 1 part 2.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,12 +1,54 @@
+use std::cmp::max;
 use std::io::{self, BufRead};
+
+fn fuel(mass: u64) -> u64 {
+    max(mass / 3, 2) - 2
+}
+
+fn total_fuel(module_fuel: u64) -> u64 {
+    let mut total = module_fuel;
+    let mut extra_fuel = fuel(total);
+    while extra_fuel > 0 {
+        total += extra_fuel;
+        extra_fuel = fuel(extra_fuel);
+    }
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn fuel_tests() {
+        assert_eq!(crate::fuel(12), 2);
+        assert_eq!(crate::fuel(14), 2);
+        assert_eq!(crate::fuel(1969), 654);
+        assert_eq!(crate::fuel(100756), 33583);
+    }
+
+    #[test]
+    fn total_fuel_tests() {
+        assert_eq!(crate::total_fuel(crate::fuel(12)), 2);
+        assert_eq!(crate::total_fuel(crate::fuel(1969)), 966);
+        assert_eq!(crate::total_fuel(crate::fuel(100756)), 50346);
+    }
+}
+
 
 fn main() {
     let stdin = io::stdin();
     let handle = stdin.lock();
-    let w: u64 = handle
+    let module_masses: Vec<u64> = handle
         .lines()
-        .map(|s| { s.unwrap().parse::<u64>() })
-        .map( |n| { (n.unwrap() / 3) - 2 })
+        .map(|s| s.unwrap().parse::<u64>().unwrap())
+        .collect();
+    let module_fuel: u64 = module_masses
+        .iter()
+        .map(|&mass| fuel(mass))
         .sum();
-    println!("{}", w);
+    println!("{}", module_fuel);
+    let total: u64 = module_masses
+        .iter()
+        .map(|&m| { total_fuel(fuel(m)) })
+        .sum();
+    println!("{}", total);
 }
