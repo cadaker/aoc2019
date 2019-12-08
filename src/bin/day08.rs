e302130402b54fab74d4165c6cedab20423c41d3
Christian Adåker <cadaker@users.noreply.github.com>
2019-12-08 11:41:14 +0100

    Day 8 part 2.

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -30,6 +30,25 @@ fn count<T: PartialEq>(haystack: &[T], needle: &T) -> usize {
         .count()
 }
 
+fn flatten_layers(image: &Image) -> Layer {
+    let mut combined = Layer::new();
+    let join = |pos| {
+        for layer in &image.layers {
+            if layer[pos] == '0' {
+                return '0';
+            } else if layer[pos] == '1' {
+                return '1';
+            }
+        }
+        '2'
+    };
+    for i in 0..image.layers.first().unwrap().len() {
+        combined.push(join(i));
+    }
+    assert_eq!(combined.len(), image.layers.first().unwrap().len());
+    combined
+}
+
 fn main() {
     let image = parse_image();
     let fewest_zeroes_layer = image
@@ -40,4 +59,31 @@ fn main() {
     let ones = count(&fewest_zeroes_layer, &'1');
     let twos = count(&fewest_zeroes_layer, &'2');
     println!("{}", ones * twos);
+
+    let flat = flatten_layers(&image);
+    for r in 0..HEIGHT {
+        for c in 0..WIDTH {
+            if flat[r*WIDTH + c] == '0' {
+                print!(" ");
+            } else {
+                print!("X");
+            }
+        }
+        println!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_flatten() {
+        let img = Image { layers: vec![
+            vec!['0', '2', '2', '2'],
+            vec!['1', '1', '2', '2'],
+            vec!['2', '2', '1', '2'],
+            vec!['0', '0', '0', '0']] };
+        let flat = flatten_layers(&img);
+        assert_eq!(flat, vec!['0', '1', '1', '0']);
+    }
 }
