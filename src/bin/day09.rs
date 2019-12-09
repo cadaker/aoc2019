be56a5b64a63687cce849429356b743c40beaf9b
Christian Adåker <cadaker@users.noreply.github.com>
2019-12-09 09:03:17 +0100

    Day 9 part 1.

---
+++ src/bin/day09.rs
@@ -0,0 +1,17 @@
+use aoc2019::intcode;
+use aoc2019::io::slurp_stdin;
+
+fn main() {
+    let program: Vec<intcode::Mem> = slurp_stdin()
+        .trim()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    {
+        let mut out = vec![];
+        intcode::run_program(program.clone(), &mut vec![1], &mut out).unwrap();
+        assert_eq!(out.len(), 1);
+        println!("{}", out[0]);
+    }
+}
