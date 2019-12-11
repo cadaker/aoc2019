d4a089a9c84fbd43e47b6ee7c941f23f9871b34d
Christian Adåker <cadaker@users.noreply.github.com>
2019-12-11 06:56:46 +0100

    Day 11 part 1.

---
+++ src/bin/day11.rs
@@ -0,0 +1,124 @@
+use std::collections::HashMap;
+use aoc2019::io::slurp_stdin;
+use aoc2019::intcode;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::ops::Deref;
+
+#[derive(Clone,Copy,PartialEq,Eq)]
+enum Color {
+    Black,
+    White,
+}
+
+const BLACK: i64 = 0;
+const WHITE: i64 = 1;
+
+#[derive(Clone,Copy,PartialEq,Eq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+#[derive(Clone,Copy,PartialEq,Eq)]
+enum Turn {
+    Left,
+    Right,
+}
+
+const LEFT: i64 = 0;
+const RIGHT: i64 = 1;
+
+struct Robot {
+    x: i32,
+    y: i32,
+    colors: HashMap<(i32,i32),Color>,
+    dir: Direction,
+}
+
+impl Robot {
+    fn new() -> Self {
+        Robot { x: 0, y: 0, colors: HashMap::new(), dir: Direction::Up }
+    }
+
+    fn trigger(&mut self, paint: Color, turn: Turn) {
+        self.colors.insert((self.x, self.y), paint);
+        self.dir = match (self.dir, turn) {
+            (Direction::Up, Turn::Left) => Direction::Left,
+            (Direction::Up, Turn::Right) => Direction::Right,
+            (Direction::Right, Turn::Left) => Direction::Up,
+            (Direction::Right, Turn::Right) => Direction::Down,
+            (Direction::Down, Turn::Left) => Direction::Right,
+            (Direction::Down, Turn::Right) => Direction::Left,
+            (Direction::Left, Turn::Left) => Direction::Down,
+            (Direction::Left, Turn::Right) => Direction::Up,
+        };
+        match self.dir {
+            Direction::Up => { self.y += 1; },
+            Direction::Right => { self.x += 1; },
+            Direction::Down => { self.y -= 1; },
+            Direction::Left => { self.x -= 1; },
+        }
+    }
+}
+
+struct RobotInput {
+    robot: Rc<RefCell<Robot>>,
+}
+
+struct RobotOutput {
+    paint_instruction: Option<Color>,
+    robot: Rc<RefCell<Robot>>,
+}
+
+impl intcode::Input for RobotInput {
+    fn next_input(&mut self) -> Result<i64, String> {
+        let robot = self.robot.deref().borrow();
+        let pos = (robot.x, robot.y);
+        match *robot.colors.get(&pos).unwrap_or(&Color::Black) {
+            Color:: Black => Ok(BLACK),
+            Color::White => Ok(WHITE),
+        }
+    }
+}
+
+impl intcode::Output for RobotOutput {
+    fn next_output(&mut self, x: i64) {
+        if self.paint_instruction.is_none() {
+            self.paint_instruction = match x {
+                BLACK => Some(Color::Black),
+                WHITE => Some(Color::White),
+                _ => unimplemented!(),
+            };
+            return;
+        }
+        let move_instruction = match x {
+            LEFT => Turn::Left,
+            RIGHT => Turn::Right,
+            _ => unimplemented!(),
+        };
+        self.robot.borrow_mut().trigger(self.paint_instruction.unwrap(), move_instruction);
+        self.paint_instruction = None;
+    }
+}
+
+
+fn main() {
+    let program: Vec<intcode::Mem> = slurp_stdin()
+        .trim()
+        .split(",")
+        .map(|s| s.parse::<intcode::Mem>().unwrap())
+        .collect();
+
+    let robot = Rc::new(RefCell::new(Robot::new()));
+    let mut input = RobotInput {robot: Rc::clone(&robot) };
+    let mut output = RobotOutput {
+        paint_instruction: None,
+        robot: Rc::clone(&robot),
+    };
+    intcode::run_program(program.clone(), &mut input, &mut output).unwrap();
+
+    println!("{}", robot.borrow_mut().colors.len());
+}
