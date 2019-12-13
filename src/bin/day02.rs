3ca931d81a2946a39a20ee94c6bd603440ee6227
Christian Adåker <cadaker@users.noreply.github.com>
2019-12-13 20:06:32 +0100

    Day 2: change to use the intcode implementation.

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,84 +1,22 @@
-use std::io::{self, Read};
+use aoc2019::intcode;
+use aoc2019::io::{parse_intcode_program, slurp_stdin};
 
-type Mem = usize;
-type IP = usize;
+fn simulate_input(mut mem: Vec<intcode::Mem>, noun: intcode::Mem, verb: intcode::Mem) -> intcode::Mem {
+    mem[1] = noun;
+    mem[2] = verb;
 
-enum State {
-    Running(IP),
-    Stopped(Mem),
-}
-
-struct Simulator {
-    memory: Vec<Mem>,
-}
-
-const ADD: Mem = 1;
-const MUL: Mem = 2;
-const END: Mem = 99;
-
-impl Simulator {
-    fn new(mem: Vec<Mem>) -> Self {
-        Simulator{memory: mem}
-    }
-
-    fn execute_one(&mut self, pc: IP) -> Result<State, (IP, Mem)> {
-        fn get_ops(mem: &Vec<Mem>, pc: IP) -> (Mem, Mem, Mem) {
-            (mem[pc+1], mem[pc+2], mem[pc+3])
-        }
-        match self.memory[pc] {
-            ADD => {
-                let (op1,op2,op3) = get_ops(&self.memory, pc);
-                self.memory[op3] = self.memory[op1] + self.memory[op2];
-                Ok(State::Running(pc+4))
-            },
-            MUL => {
-                let (op1,op2,op3) = get_ops(&self.memory, pc);
-                self.memory[op3] = self.memory[op1] * self.memory[op2];
-                Ok(State::Running(pc+4))
-            },
-            END => {
-                Ok(State::Stopped(self.memory[0]))
-            }
-            _ => Err((pc, self.memory[pc]))
-        }
-    }
-}
-
-fn simulate_input(mem: &Vec<Mem>, noun: Mem, verb: Mem) -> Mem {
-    let mut sim = Simulator::new(mem.clone());
-    sim.memory[1] = noun;
-    sim.memory[2] = verb;
-
-    let mut pc: IP = 0;
-    loop {
-        match sim.execute_one(pc).expect("invalid opcode") {
-            State::Running(next_pc) => {
-                pc = next_pc
-            },
-            State::Stopped(mem0) => {
-                break mem0
-            }
-        }
-    }
+    let out_mem = intcode::run_program(mem, &mut vec![], &mut vec![]).unwrap();
+    *out_mem.first().unwrap()
 }
 
 fn main() {
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
-    let mut buf = String::new();
-    handle.read_to_string(&mut buf).expect("read failure");
+    let memory = parse_intcode_program(&slurp_stdin());
 
-    let memory: Vec<Mem> = buf
-        .trim()
-        .split(",")
-        .map(|s| { s.parse().expect("parse failure") })
-        .collect();
-
-    println!("{}", simulate_input(&memory, 12, 02));
-    const TARGET: Mem = 19690720;
-    for noun in 0usize.. {
-        for verb in 0usize..=noun {
-            if simulate_input(&memory, noun, verb) == TARGET {
+    println!("{}", simulate_input(memory.clone(), 12, 02));
+    const TARGET: intcode::Mem = 19690720;
+    for noun in 0.. {
+        for verb in 0..=noun {
+            if simulate_input(memory.clone(), noun, verb) == TARGET {
                 println!("{}", 100*noun + verb);
                 return
             }
